16d22e16b9071a30d17e8cffb78738d30d440b9e
lopisai2 <thejefemaster117@gmail.com>
2022-11-03 14:57:49 -0600

    Instalando librerias, carpeta utils

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { firebase } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDEiNDnGFbyRO--UKnsy7myuLUB_lBCllA",
+  authDomain: "birthday-e50cb.firebaseapp.com",
+  projectId: "birthday-e50cb",
+  storageBucket: "birthday-e50cb.appspot.com",
+  messagingSenderId: "963763909950",
+  appId: "1:963763909950:web:bb3e1c604fd269d3e6686f"
+};
+
+// Initialize Firebase
+if(!firebaseConfig.apps.length) 
+{
+    firebase.initializeApp(firebaseConfig);
+}
+export default firebase;
