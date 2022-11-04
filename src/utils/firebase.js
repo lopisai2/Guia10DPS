5fdbf34b55b89ce09dcba04bd3aad64f7b7d398d
lopisai2 <thejefemaster117@gmail.com>
2022-11-03 18:12:01 -0600

    Crear componente Birthday, Firebase.js modificado

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { firebase } from "firebase/app";
+import { initializeApp } from "firebase/app";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,8 +15,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-if(!firebaseConfig.apps.length) 
-{
-    firebase.initializeApp(firebaseConfig);
-}
+
+const firebase=initializeApp(firebaseConfig);
+
 export default firebase;
+
