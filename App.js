86db665070da620e58de4bea7e7f8fec8806ca32
lopisai2 <thejefemaster117@gmail.com>
2022-11-03 19:23:39 -0600

    Logo añadido, App.js modificado

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState,useEffect} from 'react';
+import { StyleSheet,SafeAreaView,StatusBar,LogBox } from 'react-native';
+import base64 from 'react-native-base64';
+import Auth from './src/components/Auth';
+import firebase from './src/utils/firebase';
+import 'firebase/auth';
+import ListBirthday from './src/components/ListBirthday';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+function btoa(data)
+{
+  return new base64(data,"binary").toString("base64");
+}
+function atob(data)
+{
+  return new base64(data,"base64").toString("binary");
+}
+
+LogBox.ignoreAllLogs(['Settings a timer']);
+
+export default function App()
+{
+  const [user,setUser]=useState(undefined);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((response) => {
+
+      setUser(response);
+
+    })
+  },[]);
+
+  if(user===undefined)
+  {
+    return null;
+  }
+
+  return(
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.background}>
+        {user?<ListBirthday user={user}/>:<Auth/>}
+      </SafeAreaView>        
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  background: {
+    backgroundColor: '#15212b',
+    height: '100%',
+  }
 });
